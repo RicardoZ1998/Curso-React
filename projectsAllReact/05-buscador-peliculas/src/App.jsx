53e80e9558ddf8691b10d0a8171fc4f2dcd7d614
Ricardo Zuluaga <renatiko.04@gmail.com>
2024-07-23 22:03:19 -0500

    init quinto project

---
+++ projectsAllReact/05-buscador-peliculas/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
+import { Movies } from './components/Movies'
+
+function App() {
+  const movies = responseMovies.Search
+
+  return (
+    <div className='page'>
+      
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className='form'>
+          <input placeholder='Avengers, Star Wars, The Matrix...' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+
+      <main>
+        <Movies movies={movies}/>
+      </main>
+
+    </div>
+  )
+}
+
+export default App
