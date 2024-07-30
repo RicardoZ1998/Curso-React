eecd2e00f7a10ca70da0e297c49aa22d971f2c57
Ricardo Zuluaga <renatiko.04@gmail.com>
2024-07-29 22:27:22 -0500

    useMemo y useCallback

--- projectsAllReact/05-buscador-peliculas/src/App.jsx
+++ projectsAllReact/05-buscador-peliculas/src/App.jsx
@@ -36,12 +36,18 @@ function useSearch() {
 }
 
 function App() {
+  const [sort, setSort] = useState(false)
+
   const { search, updateSearch, error } = useSearch()
-  const { movies, getMovies } = useMovies({ search })
+  const { movies, loading, getMovies } = useMovies({ search, sort })
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    getMovies()
+    getMovies({ search })
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
   }
 
   const handleChange = (event) => {
@@ -58,13 +64,17 @@ function App() {
         <h1>Buscador de películas</h1>
         <form className='form' onSubmit={handleSubmit}>
           <input style={{ border: '1px solid transparent', borderColor: error ? 'red' : 'transparent' }} onChange={handleChange} value={search} name='query' placeholder='Avengers, Star Wars, The Matrix...' />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
           <button type='submit'>Buscar</button>
         </form>
         {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
-        <Movies movies={movies} />
+        {
+          loading ? <p>Loading...</p> : <Movies movies={movies} />
+        }
+        
       </main>
 
     </div>
