476a79463f75dcf64b5c2000cce3b13dfaf8959a
Ricardo Zuluaga <renatiko.04@gmail.com>
2024-07-31 23:02:11 -0500

    s

---
+++ projectsAllReact/06-shopping-cart/src/components/Filters.jsx
@@ -0,0 +1,9 @@
+export function Filters () {
+    return (
+        <section className="filters">
+            <div>
+                <label htmlFor="price">Price</label>
+            </div>
+        </section>
+    )
+}
---
+++ projectsAllReact/06-shopping-cart/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header () {
+    return(
+        <header>
+            <h1>React Shop</h1>
+            <Filters />
+        </header>
+    )
+}
